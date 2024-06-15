a5e86f023b74ca399cd3bd14cf55dfc09c2e7bc4
Alexander <82713915+EtoZheSandy@users.noreply.github.com>
2024-06-16 01:57:41 +0300

    Подправил имена переменных

--- app/src/main/java/su/afk/cocktailrecipe/data/mappers/DrinkEntitytoDrink.kt
+++ app/src/main/java/su/afk/cocktailrecipe/data/mappers/DrinkEntitytoDrink.kt
@@ -4,14 +4,6 @@ import su.afk.cocktailrecipe.data.local.entity.DrinkEntity
 import su.afk.cocktailrecipe.domain.model.Drink
 import su.afk.cocktailrecipe.domain.model.DrinkFavorite
 
-fun DrinkEntity.toDrinkFavorite(): DrinkFavorite {
-    return DrinkFavorite(
-        id = id!!,
-        nameDrink = nameDrink,
-        urlDrink = urlDrink
-    )
-}
-
 fun DrinkEntity.toDrink(): Drink {
     return Drink(
         idDrink = id!!,
