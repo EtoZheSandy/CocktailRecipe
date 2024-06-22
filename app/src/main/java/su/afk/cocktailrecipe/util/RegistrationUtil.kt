5f8e68fa11081081e20158169bb289086b63c78b
Sandy <etozhesandy@gmail.com>
2024-06-22 13:16:06 +0300

    test init

---
+++ app/src/main/java/su/afk/cocktailrecipe/util/RegistrationUtil.kt
@@ -0,0 +1,8 @@
+package su.afk.cocktailrecipe.util
+
+object RegistrationUtil {
+
+    fun registration(username: String, password: String, confirmedPassword: String): Boolean {
+        return true
+    }
+}
